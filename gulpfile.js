f42c7ca32f1e61f4e00bcfbd00dd48251a62ce7d
PaulAndrianoff <pandrianoff@gmail.com>
2017-11-19 13:21:02 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,74 @@
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      rename = require('gulp-rename'),
-      cssnano = require('gulp-cssnano'),
-      csscomb = require('gulp-csscomb'),
-      indent = require('gulp-indent'),
-      autoprefixer = require('gulp-autoprefixer'),
-      uglify = require('gulp-uglify'),
-      concat = require('gulp-concat'),
-      plumber = require('gulp-plumber'),
-      sync = require('browser-sync').create(),
-      del = require('del'),
-      syntax = require('postcss-scss'),
-      postcss    = require('gulp-postcss'),
-      sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const cssnano = require('gulp-cssnano')
+const csscomb = require('gulp-csscomb')
+const indent = require('gulp-indent')
+const autoprefixer = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const plumber = require('gulp-plumber')
+const sync = require('browser-sync').create()
+const del = require('del')
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const babelify = require('babelify')
+const browserify = require('browserify')
+const gulpif = require('gulp-if')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
 
-/*************** TASK CSS ***************/
+let isProd = process.env.NODE_ENV === 'production'
+
+/** TASK CSS ***************/
 
 gulp.task('sass', function () {
-    return gulp.src('./src/styles/sass/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/styles'))
-        .pipe(sync.stream());
-});
+  return gulp.src('./src/styles/sass/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(plumber())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({browsers: ['last 3 version'], cascade: true}))
+  .pipe(csscomb())
+  .pipe(indent({tabs: true, amount: 1}))
+  .pipe(cssnano())
+  .pipe(postcss([ require('precss'), require('autoprefixer') ]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(rename('main.min.css'))
+  .pipe(gulp.dest('./dist/styles'))
+  .pipe(sync.stream())
+})
 
-gulp.task('autoprefixer', ['sass'], function(){
+/** TASK JS ***************/
 
-    return gulp.src('./dist/styles/main.css')
-        .pipe( plumber() )
-        .pipe( autoprefixer({browsers:['last 3 version'], cascade:true}) )
-        .pipe( gulp.dest('./dist/styles') );
-});
+gulp.task('js', function () {
+  return browserify({entries: ['src/js/script.js'], debug: true})
+  .transform(babelify, {presets: 'es2015'})
+  .bundle()
+  .pipe(source('script.js'))
+  .pipe(buffer())
+  .pipe(gulpif(!isProd, sourcemaps.init({loadMaps: true})))
+  .pipe(uglify())
+  .pipe(gulpif(!isProd, sourcemaps.write('.')))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(sync.stream())
+})
+/** Main ***************/
 
-gulp.task( 'comb', ['sass', 'autoprefixer'], function(){
+// Vide le dossier dist
+function clean () {
+  gulp.task('clean', function () {
+    return del(['dist'])
+  })
+}
 
-    return gulp.src('./dist/styles/main.css')
-        .pipe( plumber() )
-        .pipe( csscomb() )
-        .pipe( gulp.dest('./dist/styles') )
-        .pipe(sync.stream());
-});
+gulp.task('build', function () {
+  clean()
+  gulp.start('sass')
+  gulp.start('js')
+})
 
-gulp.task('indentcss', ['sass', 'autoprefixer', 'comb'], function(){
+/** WATCH ***************/
 
-    return gulp.src('./dist/styles/main.css')
-        .pipe( plumber() )
-        .pipe(indent({tabs:true, amount:1}) )
-        .pipe(gulp.dest('./dist/styles') )
-        .pipe(sync.stream());
-});
-
-gulp.task( 'minify', ['sass', 'autoprefixer', 'comb', 'indentcss'], function(){
-
-    return gulp.src('./dist/styles/main.css')
-        .pipe( plumber() )
-        .pipe( cssnano() )
-        .pipe( rename('main.min.css') )
-        .pipe( gulp.dest('./dist/styles') )
-        .pipe(sync.stream());
-});
-
-gulp.task('postcss', function () {
-    return gulp.src('./dist/styles/main.css')
-        .pipe( sourcemaps.init() )
-        .pipe( postcss([ require('precss'), require('autoprefixer') ]) )
-        .pipe( sourcemaps.write('.') )
-        .pipe( gulp.dest('./dist/styles') )
-        .pipe(sync.stream());
-});
-
-/*************** TASK JS ***************/
-
-gulp.task( 'js', function(){
-
-    return gulp.src( ['./src/js/script.js', './src/js/fastclick.js'] )
-        .pipe( plumber() )
-        .pipe( concat( 'script.min.js' ) )
-        .pipe( uglify() )
-        .pipe( rename('main.min.js') )
-        .pipe( gulp.dest('./dist/js/' ) )
-        .pipe(sync.stream());
-} );
-/*************** Main ***************/
-
-//Vide le dossier dist
-function clean() {
-    gulp.task( 'clean', function(){
-        return del(['dist']);
-    } )
-} ;
-
-gulp.task('build', function(){
-    // clean();
-    // gulp.start('clean');
-    gulp.start('sass');
-    gulp.start('comb');
-    gulp.start('indentcss');
-    gulp.start('minify');
-    gulp.start('js');
-    gulp.start('postcss');
-    // gulp.start('postcss');
-});
-
-/*************** WATCH ***************/
-
-gulp.task ( 'default', function(){
-
-    gulp.watch ('./src/**/*.scss',['sass','minify','comb','indentcss','postcss'])
-    gulp.watch ('./src/js/script.js', ['js'])
-});
-
+gulp.task('default', function () {
+  gulp.watch('./src/**/*.scss', ['sass'])
+  gulp.watch('./src/js/script.js', ['js'])
+})
